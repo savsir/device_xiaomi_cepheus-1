dee45c0e5490fe2f65c3b2b84c7833002136db69
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    Import vendor/qcom/opensource/commonsys/packages/apps/Bluetooth soong namespace

--- device.mk
+++ device.mk
@@ -428,7 +428,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Telephony
 PRODUCT_PACKAGES += \
