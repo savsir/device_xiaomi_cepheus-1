807eba6c7c0a779fe2d7edeca006cb2948a7e214
savsir <svsrcnkt@gmail.com>
2021-01-24 22:39:03 +0300

    Switch AOSP bt

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -1,8 +1,11 @@
 /*
+ *
  *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
- *  Not a Contribution.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
  *
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2018-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,13 +23,49 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
+#include <cutils/properties.h>
+#include <string.h>
+
+#include "osi/include/osi.h"
+
+typedef struct {
+    const char *product_device;
+    const char *product_model;
+} device_t;
+
+static const device_t devices[] = {
+    {"cepheus", "Xiaomi Mi 9"},
+};
+
+static inline const char *BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
+        device_t device = devices[i];
+
+        if (strcmp(device.product_device, product_device) == 0) {
+            return device.product_model;
+        }
+    }
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-// skips conn update at conn completion
+// Skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
+// Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
+#define AVDT_NUM_SEPS 12
 
-#define AVDT_NUM_SEPS 35
+#pragma pop_macro("PROPERTY_VALUE_MAX")
+
 #endif
