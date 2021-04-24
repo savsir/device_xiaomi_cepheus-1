f063ea23bee04f50ccd0311eeae6ef59e330d45c
Savsir <svsrcnkt@gmail.com>
2021-04-24 21:00:18 +0300

    cepheus: Enable blurs default

--- device.mk
+++ device.mk
@@ -176,6 +176,7 @@ PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
 
 # Fingerprint
